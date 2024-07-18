d4bf0041584d6063e4c6c96fefcc3adf535996c5
Jodie Mullins <jmullins2460@gmail.com>
2024-07-18 19:19:21 -0400

    copy over -- starting point

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # CY-Product_Website
+
+
+
+
+This repository utilizes HTML, CSS, and vanilla JavaScript to create what a website may look like across browser and mobile views. 
+
+* Responsive Design {Best Practice} [All Pages]
+
+* Integrate a third-party API into project. {REQ}
+
+
+
+
+
+
+Guideline Features: 
+
+* Use arrays, objects, sets or maps to store and retrieve information that is displayed in your app. {Easy} [Seasonal]
+
+* Convert user input between two formats and display the result. (e.g. Fahrenheit to Celcius, kilograms to pounds, etc) {Intermediate} [Home]
+
+* Calculate and display data based on an external factor (ex: get the current date, and display how many days remaining until some event) {Intermediate} [Promo]
+
+* Retrieve data from a third-party API and use it to display something within your app. {Easy} [https://fakestoreapi.com/]
---
+++ jscript.js
@@ -0,0 +1,144 @@
+'use strict';
+
+/***
+ * 
+ *          HOME PAGE 
+ * 
+ ***/
+
+function temperatureConverter(valNum) {
+
+    // convert user string input to float
+    valNum = parseFloat(valNum);
+
+    // grab this item and display the output of 
+    // an equation to translate Celcius into Fahrenheit 
+    document.getElementById("outputFahrenheit").innerText=(valNum * 1.8) +32 ;
+};
+
+
+
+
+
+
+
+
+
+/***
+ * 
+ *          SEASONAL / DESIGNS 
+ * 
+ ***/
+
+// Where do I want to pull this image from? 
+// const crim = 
+
+// on click, I want the image to shrink or otherwise thumbnail 
+// on click, name and min bid price should appear
+
+// or, or , or, ORRRRRR
+// on click
+// Create 2 rectangles overlapping the shoe image
+// name and min bid price
+
+// pull array of images
+// create image objects complete with alt txt
+
+// >>> MOVE alt txt to API descriptions
+
+
+
+
+
+/** 
+// create javascript variable
+// set javascript variable equal to an array
+// make the array contain a single image of the title
+const imgdata = ['GGG-1'];
+
+// create javascript object that grabs an Id
+// 
+const imgContainer = document.getElementById('seasonal-images');
+
+const bidImages = (imgs) => {
+    imgs.forEach(img => {
+        const parentdiv = document.createElement('div')
+        parentdiv.id = 'div' + img
+        parentdiv.className = 'divs-seasonal-images'
+        
+        // create image with id
+        const seasonalImage = document.createElement('img')
+        seasonalImage.id = img
+        seasonalImage.class = 'seasonal-selections'
+        seasonalImage.src = 'img/' + img + '.jpg'
+
+        parentdiv.appendChild(seasonalImage)
+        imgContainer.appendChild(parentdiv)
+    });
+
+};
+
+// call function on array
+bidImages(imgdata);
+
+*/
+
+
+
+/** 
+<div id="div-ggg1" class="divs-seasonal-images">
+ //  create object to add alt 
+    <img id="GGG-1" class="seasonal-selections" src="img/GGG-1.jpg" alt="AI-generated pair of open-toe pumps with red interior, tan exterior soles; heels are gradient dark to light blue with varying white, light blue, dark blue, to black straps covered in hand-painted floral designs of oranges, blues, lavenders. and creams." />
+</div> 
+*/
+
+
+
+
+
+
+
+/***
+ * 
+ *          PROMO 
+ * 
+ ***/
+
+// grab today's date
+let today = new Date();
+// detail today's year
+let saleYear = today.getFullYear();
+
+// if the date is after October 31st (so any time November 1 and later)
+if (today.getMonth() == 9 && today.getDate() > 31) {
+
+    // add another year until the sale date
+    saleYear = saleYear + 1;
+};
+
+// if today is Halloween exactly
+if (today.getMonth() == 9 && today.getDate() === 31) {
+
+    // box should have a message
+   let saleMessage = "TODAY IS THE SALE! CHECK OUT THE SEASONAL PAGE!"
+   
+   // display message exclaiming day of sale
+   document.getElementById("countdown-here").innerText = saleMessage;
+};
+
+// create variable saleDate and set it equal to October 31st of closest upcoming sale
+let saleDate = new Date(saleYear, 9, 31);
+
+// calculating ms total of time in a day
+let dayMilliseconds = 1000 * 60 * 60 * 24;
+
+// Find remaining days until next sale
+let remainingDays = Math.ceil(
+    (saleDate.getTime() - today.getTime()) / (dayMilliseconds)
+);
+
+// let todayMessage = "Today is " + today + "."
+
+let saleMessage = ("There are " + remainingDays + " days remaining until the next HUGE Halloween sale!");
+
+document.getElementById("countdown-here").innerText = saleMessage;
