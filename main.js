61fe9c5a610d18158db0bc161bb1911870f69cc8
Jodie Mullins <jmullins2460@gmail.com>
2024-07-22 15:16:08 -0400

    cohesion

--- main.js
+++ main.js
@@ -15,9 +15,24 @@
 
 async function standardProductListingFiveItems() {
 
-    fiveItemsListedURL = 'https://fakestoreapi.com/products?limit=5'
 
+    const fiveItemsListedURL = 'https://fakestoreapi.com/products?limit=5';
+
+    try {
+        const responseForFiveItems = await fetch(fiveItemsListedURL);
+
+        // error handling
+        if ()
+    }
 }
+
+
+
+
 fetch(fiveItemsListedURL)
             .then(res=>res.json())
+
+
+
+const apiContainer = document.getElementById('')
             
