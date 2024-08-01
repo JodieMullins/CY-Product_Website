c9bb54fa91a4bbd7772a15ab1e3074bb106bbfe1
Jodie Mullins <jmullins2460@gmail.com>
2024-07-31 22:37:14 -0400

    lighten the error handling

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ routes.get("/", (req, res) => {
 let shoeInfo = require('./info/shoeInfo');
 
 // bring in export in case of errors
-let errorHelper = require('./helpers/errorHelpers');
+// let errorHelper = require('./helpers/errorHelpers');
 
 // use the express Router object
 const router = express.Router();
@@ -158,99 +158,6 @@ router.post('/', function (req, res, next) {
 });
 
 
-router.put('/:id', function (req, res, next) {
-  // goes and grabs the shoe value
-  shoeInfo.getById(req.params.id, function (data) {
-    if (data) {
-      // Attempt to update the data if found
-      shoeInfo.update(req.body, req.params.id, function (data) {
-        res.status(200).json({
-          "status": 200,
-          "statusText": "OK",
-          "message": "Shoes '" + req.params.id + "' updated.",
-          "data": data
-        });
-      });
-    }
-    else {
-      // if shoe data was not found / nonexistent, return a 404 error code
-      res.status(404).send({
-        "status": 404,
-        "statusText": "Not Found",
-        "message": "The pair of shoes '" + req.params.id + "' could not be found.",
-        "error": {
-          "code": "NOT_FOUND",
-          "message": "The pair of shoes '" + req.params.id + "' could not be found."
-        }
-      });
-    }
-  }, function(err) {
-    next(err);
-  });
-});
-
-// create delete response
-router.delete('/:id', function (req, res, next) {
-  shoeInfo.getById(req.params.id, function (data) {
-    if (data) {
-      // Attempt to delete the data
-      shoeInfo.delete(req.params.id, function (data) {
-        res.status(200).json({
-          "status": 200,
-          "statusText": "OK",
-          "message": "The shoes '" + req.params.id + "' have been deleted.",
-          "data": "Shoes '" + req.params.id + "' deleted."
-        });
-      });
-    }
-    else {
-      res.status(404).send({
-        "status": 404,
-        "statusText": "Not Found",
-        "message": "The shoes '" + req.params.id + "' could not be found.",
-        "error": {
-          "code": "NOT_FOUND",
-          "message": "The shoes '" + req.params.id + "' could not be found."
-        }
-      });
-    }
-  }, function(err) {
-    next(err);
-  });
-});
-
-
-router.patch('/:id', function (req, res, next) {
-  // look for shoe id pass through as parameter
-  shoeInfo.getById(req.params.id, function (data) {
-    if (data) {
-      // Attempt to update the data
-      // Will only update specific properties 
-      shoeInfo.update(req.body, req.params.id, function (data) {
-        res.status(200).json({
-          "status": 200,
-          "statusText": "OK",
-          "message": "Shoes '" + req.params.id + "' patched.",
-          "data": data
-        });
-      });
-    }
-    else {
-      res.status(404).send({
-        "status": 404,
-        "statusText": "Not Found",
-        "message": "The shoes '" + req.params.id + "' could not be found.",
-        "error": {
-          "code": "NOT_FOUND",
-          "message": "The shoes '" + req.params.id + "' could not be found."
-        }
-      });
-    }
-  }, function (err) {
-    next(err);
-  });
-});
-
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api/', router);
 // ALL REST APIs in this server are called:
@@ -258,24 +165,6 @@ app.use('/api/', router);
 
 
 
-
-/**
- * 
- *  MOVE TO errorHelpers.js
- * 
-function errorBuilder(err) {
-  return {
-    "status": 500,
-    "statusText": "Internal Server Error",
-    "message": err.message,
-    "error": {
-      "errno": err.errno,
-      "call": err.syscall,
-      "code": "INTERNAL_SERVER_ERROR",
-      "message": err.message
-    }
-};
-};
 
 // ADD EXCEPTION HANDLING
 // EXCEPTION HANDLING NEEDS TO BE LAST, BEFORE SERVER APP
@@ -294,36 +183,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-*/
-
-
-// Configure exception logger
-// WHY DOES IT SAY app.use(errorHelper.logErrorsToConsole);   on Pluralsight???????????????????
-//app.use(errorHelper.logErrors);
-//app.use(errorHelper.logErrorsToConsole);
-
-/** 
- *    IS THIS HOW I WOULD CALL PROMO PAGE ORDINARILY??
- * 
-app.get('/promo', (req, res)=>{
-  res.set('Content-Type', 'text/html');
-  res.status(200).send('./promo.html');
-});
-*/
-
-
-// Configure exception logger to file
-app.use(errorHelper.logErrorsToFile);
-
-
-
-// Configure client error handler
-app.use(errorHelper.clientErrorHandler);
-
-
-
-// Configure catch-all exception middleware last
-app.use(errorHelper.errorHandler);
 
 
 
